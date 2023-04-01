4997d396c2bfe388ffc747ba8eec7b2bb2100b71
Rhythm2511 <mustakim.rhythm@gmail.com>
2023-04-02 03:40:39 +0600

    signup & signin

--- pages/signin.js
+++ pages/signin.js
@@ -5,16 +5,19 @@ export default function(){
         <>
         <div>
             <h1>Sign In</h1>
-            <form>
-                
+            <br />
+            <form>  
+                <fieldset>
+                    <br />
                 <label htmlFor="email">Email:</label> &nbsp;
-                <input type="email" id="email" name ="email"/><br></br><br></br>
+                <input type="email" id="email" name ="email"/> <br /><br />
            
                 <label htmlFor="password">Password:</label> &nbsp;
-                <input type="password" name ="password"/><br></br><br></br>
+                <input type="password" name ="password"/> <br /><br />
 
                 <button>Sign In</button>
-    
+                <br /> <br />
+                </fieldset>
             </form>          
             <br></br>
             <p>Don't have an account yet? <Link href="/signup">Sign Up</Link></p>
--- pages/signup.js
+++ pages/signup.js
@@ -6,16 +6,30 @@ export default function SignUp(){
         <div>
             <h1>SignUp</h1><br></br>
             <form>
+                <fieldset>
+                <br />
                 <label htmlFor="name">Name:</label> &nbsp;
-                <input type="text" name ="name"/><br></br><br></br>
-           
+                <input type="text" name ="name" placeholder="Enter your name"/> <br /> <br />
+
+                <label htmlFor="dob">Date of Birth:</label> &nbsp;
+                <input type="date" name="dob"/><br /><br />
+
+                <label htmlFor="gender">Gender:</label> &nbsp;
+                <input type="radio" name="gender" id="male" value="Male" />Male &nbsp;
+                <input type="radio" name="gender" id="female" value="Female" />Female <br /><br />
+                
+                <label htmlFor="phone">Phone Number: </label> &nbsp;
+                <input type="tel" name="phone" placeholder="Enter your phone number" /><br /> <br />
+
                 <label htmlFor="email">Email:</label> &nbsp;
-                <input type="email" id="email" name ="email"/><br></br><br></br>
+                <input type="email" id="email" name ="email" placeholder="Enter your email address"/><br /><br />
            
                 <label htmlFor="password">Password:</label> &nbsp;
-                <input type="password" name ="password"/><br></br><br></br>     
+                <input type="password" name ="password" placeholder="Enter your password"/><br /><br />  
 
                 <button>Sign Up</button>
+                <br /><br />
+                </fieldset>
             </form>
             <br></br>
             <p>Already have an account? <Link href="/signin">Sign In</Link></p>
